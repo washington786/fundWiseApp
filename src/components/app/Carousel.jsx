2ebd1a8ec3bbdeaa332d38681ea22489927dc838
washington786 <dkmawasha@gmail.com>
2024-10-27 00:01:03 +0200

    v3

--- src/components/app/Carousel.jsx
+++ src/components/app/Carousel.jsx
@@ -1,26 +1,14 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
 
-import { SliderBox } from "react-native-image-slider-box";
-
-const images = [
-  "https://source.unsplash.com/1024x768/?nature",
-  "https://source.unsplash.com/1024x768/?water",
-  "https://source.unsplash.com/1024x768/?girl",
-  "https://source.unsplash.com/1024x768/?tree",
-];
 const Carousel = () => {
   return (
-    <SliderBox
-      images={images}
-      sliderBoxHeight={200}
-      onCurrentImagePressed={(index) => console.warn(`image ${index} pressed`)}
-      dotColor="#FFEE58"
-      inactiveDotColor="#90A4AE"
-    />
-  );
-};
+    <View>
+      <Text>Carousel</Text>
+    </View>
+  )
+}
 
-export default Carousel;
+export default Carousel
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({})
